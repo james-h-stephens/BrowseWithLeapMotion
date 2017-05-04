8ca330e6b8d249d7324d8bd83d7a26c45f0a6031
James <james.h.stephens@protonmail.com>
2017-05-03 20:25:24 -0400

    Added additional settings

--- Google Chrome/settings.js
+++ Google Chrome/settings.js
@@ -2,5 +2,8 @@ var settings = {
     scrollMax: 50,
     scrollHand: "right",
     scrollDownBounds: [-.25, -.6],
-    scrollUpBounds: [.5, 1]
+    scrollUpBounds: [.5, 1],
+    pointerColor: "rgba(255,0,0,.5)",
+    clickerColor: "rgba(0,0,255,.5)",
+    clickDelay: 1000
 }
