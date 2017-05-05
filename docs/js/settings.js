8d9240ce84b033a675b4c269bb1347b4c83b3c7f
James <james.h.stephens@protonmail.com>
2017-05-04 21:21:30 -0400

    Promoting code to lib

--- docs/js/settings.js
+++
@@ -1,9 +0,0 @@
-var settings = {
-    scrollMax: 50,
-    scrollHand: "right",
-    scrollDownBounds: [-.25, -.6],
-    scrollUpBounds: [.5, 1],
-    pointerColor: "rgba(255,0,0,.5)",
-    clickerColor: "rgba(0,0,255,.5)",
-    clickDelay: 1000
-}
